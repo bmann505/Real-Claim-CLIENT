bbf156292a7b4d657a481335f381c1259b0b85e1
Brian Mann <bmann505@gamil.com>
2017-10-26 15:35:32 -0600

    deploy route

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -7,23 +7,24 @@ export class UserService {
   constructor(private http: Http) {}
 
   // 'https://radiant-wave-93298.herokuapp.com/user'
+  // http://localhost:8080/adjustor/
     getUsers() {
-      return this.http.get('http://localhost:8080/user')
+      return this.http.get('https://radiant-wave-93298.herokuapp.com/owner')
     }
 
     postClaim(body: any) {
-      return this.http.post('http://localhost:8080/claim', body)
+      return this.http.post('https://radiant-wave-93298.herokuapp.com/claim', body)
     }
 
     getClaimsByOwner(id) {
-      return this.http.get(`http://localhost:8080/owner/${id}`)
+      return this.http.get(`https://radiant-wave-93298.herokuapp.com/owner${id}`)
     }
 
     getClaimsByContractor(id) {
-      return this.http.get(`http://localhost:8080/contractor/${id}`)
+      return this.http.get(`https://radiant-wave-93298.herokuapp.com/contractor${id}`)
     }
     getClaimsByAdjustor(id) {
-      return this.http.get(`http://localhost:8080/adjustor/${id}`)
+      return this.http.get(`https://radiant-wave-93298.herokuapp.com/adjustor${id}`)
     }
 
     parsedJWT(token) {
